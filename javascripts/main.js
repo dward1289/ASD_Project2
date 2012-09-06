0d8244f2106b6579d86763845636f99e0f20b151
dward1289 <dward1289@fullsail.edu>
2012-09-06 17:13:39 -0300

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,569 @@
-console.log('This would be the main JS file.');
+//Wait until the DOM is ready
+$("#home").on('pageinit', function(){
+
+
+var jsonD = function () {
+$.ajax({
+  	type: "GET",
+		url: "data/saved.json",
+		dataType: "json",
+		success: function (result) {
+
+                for ( var i = 0, len = result.tasks.length; i < len; i++ ) {
+                    var item = result.tasks[i];
+
+                    $( ' ' + 
+					'<div class="tasksIn">' +
+					'<ul>' +
+					'<li> Name of Task: ' + item.name[1] + '</li>' +
+					'<li> Category: ' + item.category[1] + '</li>' +
+					'<li> Priority: ' + item.priorityLevel[1] + '</li>' +
+					'<li> Start: ' + item.startUp[1] + '</li>' +
+					'<li> End: ' + item.ending[1] + '</li>' +
+					'<li> Alert: ' + item.alertOption[1] + '</li>' +
+					'<li> Note: ' + item.note[1] + '</li>' +
+					'</ul>' +
+					'<hr />' +
+					'</div>'
+					).appendTo("#thisJSON");
+                }
+            }
+				
+		});
+}
+	$("#jsonB").on("click", jsonD);
+
+
+var xmlD = function () {
+$.ajax({
+		type: "GET",
+		url: "data/savedtasks.xml",
+		dataType: "xml",
+		success: function (xml) {
+			$(xml).find('item').each(function() {
+				var xName = $(this).find('name').text();
+				var xCategory = $(this).find('category').text();
+				var xPriority = $(this).find('priority').text();
+				var xStart = $(this).find('start').text();
+				var xEnd = $(this).find('ending').text();
+				var xAlert = $(this).find('alert').text();
+				var xNote = $(this).find('note').text();
+				$(' '+
+				'<div class="tasksIn">' +
+					'<ul>' +
+					'<li> Name of Task: ' + xName + '</li>' +
+					'<li> Category: ' + xCategory+ '</li>' +
+					'<li> Priority: ' + xPriority + '</li>' +
+					'<li> Start: ' + xStart + '</li>' +
+					'<li> End: ' + xEnd + '</li>' +
+					'<li> Alert: ' + xAlert + '</li>' +
+					'<li> Note: ' + xNote + '</li>' +
+					'</ul>' +
+					'<hr />' +
+					'</div>'
+					).appendTo("#thisXML");
+			}
+		)}
+		
+		});
+}
+	$("#xml").on("click", xmlD);
+
+var csvD = function () {
+$.ajax({
+		type: "GET",
+		url: "data/savedlist.csv",
+		dataType: "text",
+		success: function (data) {
+			var line = data.split("\n");
+			var cRow = line[0];
+			var cCol = cRow.split(",");
+			for(var lnum = 1; lnum<line.length; lnum++){
+				var row = line[lnum];
+				var column = row.split(",");
+			$(' '+
+					'<div class="taskIn">' +
+					'<ul>' +
+					'<li>' + cCol[0] + " " + column[0] + '</li>' +
+					'<li>' + cCol[1] + " " + column[1] + '</li>' +
+					'<li>' + cCol[2] + " " + column[2] + '</li>' +
+					'<li>' + cCol[3] + " " + column[3] + '</li>' +
+					'<li>' + cCol[4] + " " + column[4] + '</li>' +
+					'<li>' + cCol[5] + " " + column[5] + '</li>' +
+					'<li>' + cCol[6] + " " + column[6] + '</li>' +
+					'</ul>' +
+					'<hr />' +
+					'</div>'
+					).appendTo("#thisCSV");
+			
+			}
+			
+			}
+		
+		});
+		}
+
+	$("#csv").on("click", csvD);
+	
+	//getElementById function
+	var elId = function (n) {
+		var theElement = document.getElementById(n);
+		return theElement;
+	}
+	
+	
+	//Find value of selected radio button.
+	var radiobox = function () {
+		var radios = document.forms[0].whichCategory;
+		for(var i=0; i<radios.length; i++) {
+			if(radios[i].checked) {
+				whichCategoryValue = radios[i].value;
+				}
+			}
+		}
+	
+	
+	
+	var toggleContr = function (n) {
+		switch(n) {
+			case "on":
+				$('#displayData2').css("display", "none");
+				break;
+			case "off":
+				$('#displayData2').css("display", "inline");
+				$('#items').css("display", "none");
+				
+				break;
+			default:
+				return false;
+				}
+			}
+			
+	
+	//Store data function
+	var storeData = function (key) {
+		//No key = new key
+		if(!key){
+			var id = Math.floor(Math.random()*10000000001);
+			}
+			else{
+				//Existing key will be saved when edited
+				id = key;
+				}
+	}
+	
+	
+	//Get data function
+	var getData = function () {
+		toggleContr("on");
+		if(localStorage.length === 0) {
+			alert("There is no data in storage. Default data has been added.");
+			autoFillData();
+			}
+			
+		//Write data from local storage to browser
+		var makeDiv = $("<div></div>").attr("id", "items");
+		var makeList = $("<ul></ul>").attr("id", "wholeList");
+		makeDiv.html(makeList);
+		$('#seeHere').html(makeDiv);
+		$('#items').css("display", "block");
+		for(var i=0, len=localStorage.length; i<len; i++) {
+			var makeLi = $("<li></li>").attr("id", "listing");
+			var linksLi = $("<li></li>");
+			makeList.append(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+		
+		//Convert string from local to object
+			var obj = JSON.parse(value);
+			var makeSubList = $("<ul></ul>");
+			makeLi.append(makeSubList);
+			getImage(obj.priorityLevel[1], makeSubList);
+			for(var r in obj) {
+				var makeSubLi = $("<li></li>");
+				makeSubList.append(makeSubLi);
+				var optSubText = obj[r][0]+" "+obj[r][1];
+				makeSubLi.html(optSubText);
+				makeSubList.append(linksLi);
+				
+				}
+				
+				//Create edit and delete buttons for items in local storage
+				makeItemLinks(localStorage.key(i), linksLi); 
+		}
+	}
+	
+	//Image for categories
+	var getImage = function (catName, makeSubList) {
+			var imgLi = $("<li></li>");
+		makeSubList.html(imgLi);
+		var newImg = $("<img></img>").attr("src", "images/"+ catName + ".png");
+		imgLi.html(newImg);
+	}
+	
+	
+	var autoFillData = function () {
+		//JSON object comes from json.js, storing it in local storage.
+		for(var n in json){
+			var id = Math.floor(Math.random()*10000000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}
+	
+	//Make edit and delete buttons for each stored item
+	var makeItemLinks = function (key, linksLi) {
+		//add edit single item link
+		var edit = $("<a></a>").attr("href", "#").text("Edit Task");
+		edit.key = key;
+		edit.on("click", editItem);
+		linksLi.html(edit);
+		
+			//add line break
+		var breakIt = $("<br></br>");
+		linksLi.html(breakIt);
+		
+		//add delete single link
+		var deleteIt = $("<a></a>").attr("href", "#").text("Delete Task");
+		deleteIt.key = key;
+		
+		deleteIt.on("click", deleteItem);
+		linksLi.html(deleteIt);
+
+		}
+	
+	var editItem = function () {
+		//Grab the data first.
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//Show form again
+		toggleContr("off");
+		
+		//populate form
+		$('#taskName').val(item.name[1]);
+		$('#priorities').val(item.priorityLevel[1]);
+		$('#taskDate').val(item.startUp[1]);
+		$('#taskEnd').val(item.ending[1]);
+		$('#alertWay').val(item.alertOption[1]);
+		$('#notes').val(item.note[1]);
+		if(item.category[1] == "Home") {
+			$('#home').attr("checked", "checked");
+			}
+		if(item.category[1] == "Business") {
+			$('#business').attr("checked", "checked");
+					}
+		if(item.category[1] == "School") {
+			$('#school').attr("checked", "checked");
+					}
+		
+		
+
+		
+		var editSubmit = $('#submit');
+		editSubmit.on("click", validate);
+		editSubmit.key = this.key;	
+	}
+		
+	//Set Link & Submit Click Events
+	var displayLink2 = $('#displayData2');
+	displayLink2.on("click", getData);
+	
+	var deleteItem = function () {
+		var ask = confirm("Are you sure you want to delete this task?");
+		alert("Task deleted.");
+		if(ask){
+			localStorage.removeItem(this.key);
+			window.location.reload();
+		}
+		else{
+			alert("Task not deleted.");
+			window.location.reload();
+			return false;
+		}
+	}
+
+});
+	
+
+//Wait until the DOM is ready
+$("#additem").on('pageinit', function(){
+
+	var validator = function () {
+	$('#taskForm').validate();
+	}
+	
+	//Find value of selected radio button.
+	var radiobox = function () {
+		var radios = document.forms[0].whichCategory;
+		for(var i=0; i<radios.length; i++) {
+			if(radios[i].checked) {
+				whichCategoryValue = radios[i].value;
+				}
+			}
+		}
+	
+	
+	var toggleContr = function (n) {
+		switch(n) {
+			case "on":
+				$('#taskForm').css("display", "none");
+				$('#clear').css("display", "inline");
+				$('#displayData').css("display", "none");
+				break;
+			case "off":
+				$("taskForm").css("display", "block");
+				$("clear").css("display", "inline");
+				$("displayData").css("display", "inline");
+				$("items").css("display", "none");
+				
+				break;
+			default:
+				return false;
+				}
+			}
+			
+	
+	//Store data function
+	var storeData = function (key) {
+		//No key = new key
+		if(!key){
+			var id = Math.floor(Math.random()*10000000001);
+			}
+			else{
+				//Existing key will be saved when edited
+				id = key;
+				}
+				
+		//Get all form field values and store in object
+		//Object properties contain array w/from label and input value
+		radiobox();
+		var item = {};
+		item.name = ["Name of Task: ", $('#taskName').val()];
+		item.category = ["Category: ", whichCategoryValue];
+		item.priorityLevel = ["Priority: ", $('#priorities').val()];
+		item.startUp = ["Starting Date of Task: ", $('#taskDate').val()];
+		item.ending = ["Ending Date of Task: ", $('#taskEnd').val()];
+		item.alertOption = ["Type of Alert: ", $('#alertWay').val()];
+		item.note = ["Notes", $('#notes').val()];
+	
+		//Save data into Local Storage: stringify to convert object to string
+		localStorage.setItem(id, JSON.stringify(item));		
+		alert("Task Saved!");
+		window.location.reload();
+	}
+	
+	
+	//Get data function
+	var getData = function () {
+		toggleContr("on");
+		if(localStorage.length === 0) {
+			alert("There is no data in storage. Default data has been added.");
+			autoFillData();
+			}
+			
+		//Write data from local storage to browser
+		var makeDiv = $("<div></div>").attr("id", "items");
+		var makeList = $("<ul></ul>").attr("id", "wholeList");
+		makeDiv.html(makeList);
+		$('#seeHere').html(makeDiv);
+		$('#items').css("display", "block");
+		for(var i=0, len=localStorage.length; i<len; i++) {
+			var makeLi = $("<li></li>").attr("id", "listing");
+			var linksLi = $("<li></li>");
+			makeList.append(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+		
+		//Convert string from local to object
+			var obj = JSON.parse(value);
+			var makeSubList = $("<ul></ul>");
+			makeLi.append(makeSubList);
+			getImage(obj.priorityLevel[1], makeSubList);
+			for(var r in obj) {
+				var makeSubLi = $("<li></li>");
+				makeSubList.append(makeSubLi);
+				var optSubText = obj[r][0]+" "+obj[r][1];
+				makeSubLi.html(optSubText);
+				makeSubList.append(linksLi);
+				
+				}
+				
+				//Create edit and delete buttons for items in local storage
+				makeItemLinks(localStorage.key(i), linksLi); 
+		}
+	}
+	
+	//Image for categories
+	var getImage = function (catName, makeSubList) {
+		var imgLi = $("<li></li>");
+		makeSubList.html(imgLi);
+		var newImg = $("<img></img>").attr("src", "images/"+ catName + ".png");
+		imgLi.html(newImg);
+	}
+	
+	var autoFillData = function () {
+		//JSON object comes from json.js, storing it in local storage.
+		for(var n in json){
+			var id = Math.floor(Math.random()*10000000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}
+	
+var validator = function (e) {
+		//Define elements
+		var getPriority = $('#priorities');
+		var getNot = $('#taskName');
+		var getStart = $('#taskDate');
+		var getEnd = $('#taskEnd');
+		
+		//Reset error messages
+		errMsg.html("");
+		getPriority.css("border", "1px solid black");
+		getNot.css("border", "1px solid black");
+		getStart.css("border", "1px solid black");
+		getEnd.css("border", "1px solid black");
+
+
+		//Error messages array
+		var message = [];
+		
+		//Priority validate
+		if(getPriority.val() === "--Choose Priority Level--") {
+			var priorityError = "Please select priority level.".fontcolor("red").bold();
+			getPriority.css("border", "2px solid red");
+			message.push(priorityError);
+		}
+		//Name of Task validate
+		if(getNot.val() === "") {
+			var notError = "Please enter the name of task.".fontcolor("red").bold();
+			getNot.css("border", "2px solid red");
+			message.push(notError);
+		}
+		//Start date validate
+		if(getStart.val() === "") {
+			var startError = "Please select a start date.".fontcolor("red").bold();
+			getStart.css("border", "2px solid red");
+			message.push(startError);
+		}
+		//End date validate
+		if(getEnd.val() === "") {
+			var endError = "Please select an ending date.".fontcolor("red").bold();
+			getEnd.css("border", "2px solid red");
+			message.push(endError);
+		}
+		//Explains errors
+		if(message.length >=1) {
+			for(var i = 0, j = message. length; i < j; i++){
+				var txt = $("<li></li>");
+				txt.html(message[i]);
+				errMsg.appendTo(txt);
+			}
+		e.preventDefault();
+		return false;	
+		}
+		else{
+			storeData(this.key);
+			}		
+	}
+	
+	//Make edit and delete buttons for each stored item
+	var makeItemLinks = function (key, linksLi) {
+		//add edit single item link
+		var edit = $("<a></a>").attr("href", "#").text("Edit Task");
+		edit.key = key;
+		edit.on("click", editItem);
+		linksLi.html(edit);
+		
+		//add line break
+		var breakIt = $("<br></br>");
+		linksLi.html(breakIt);
+		
+		//add delete single link
+		var deleteIt = $("<a></a>").attr("href", "#").text("Delete Task");
+		deleteIt.key = key;
+		
+		deleteIt.on("click", deleteItem);
+		linksLi.html(deleteIt);
+		}
+	
+	var editItem = function () {
+		//Grab the data first.
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//Show form again
+		toggleContr("off");
+		
+		//Populate with current
+		$('#taskName').val(item.name[1]);
+		$('#priorities').val(item.priorityLevel[1]);
+		$('#taskDate').val(item.startUp[1]);
+		$('#taskEnd').val(item.ending[1]);
+		$('#alertWay').val(item.alertOption[1]);
+		$('#notes').val(item.note[1]);
+		if(item.category[1] == "Home") {
+			$('#home').attr("checked", "checked");
+			}
+		if(item.category[1] == "Business") {
+			$('#business').attr("checked", "checked");
+					}
+		if(item.category[1] == "School") {
+			$('#school').attr("checked", "checked");
+					}
+		
+		
+		//Remove listener from submit button.
+		submit1.off("click", storeData);
+		
+		//Change submit value to edit
+		//Found helpful code for button at: http://www.permadi.com/tutorial/jsInnerHTMLDOM/index.html
+		$('#submit').val("Edit Task");
+		var editSubmit = $('#submit');
+		
+		//Save key value in this function as property of editSubmit, use that value when save edited data.
+		editSubmit.on("click", validate);
+		editSubmit.key = this.key;
+	}
+	
+	
+	var deleteItem = function () {
+		var ask = confirm("Are you sure you want to delete this task?");
+		alert("Task deleted.");
+		if(ask){
+			localStorage.removeItem(this.key);
+			window.location.reload();
+		}
+		else{
+			alert("Task not deleted.");
+			window.location.reload();
+			return false;
+		}
+	}
+	
+	var clearLocal = function () {
+		if(localStorage.length === 0){
+			alert("There is no data to clear.")
+		}
+		else{
+			localStorage.clear();
+			alert("All tasks have been cleared.");
+			window.location.reload();
+			return false;
+		}
+	
+	}
+		
+	//Variable defaults
+	var priorityGroup = ["--Choose Priority Level--","High","Medium","Low"];
+	var whichCategoryValue;
+	errMsg = $('#errors');
+	
+	//Set Link & Submit Click Events
+	var displayLink = $('#displayData');
+	displayLink.on("click", getData);
+	var clearLink = $('#clear');
+	clearLink.on("click", clearLocal);
+	var submit1 = $('#submit');
+	submit1.on("click", validator);
+	
+});
